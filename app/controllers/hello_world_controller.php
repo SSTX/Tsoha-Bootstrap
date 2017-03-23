6be039385e475cf22cd9fc867a52520a25086ca8
ttiira <taneli.tiira@helsinki.fi>
2017-03-23 18:43:24 +0200

    html

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,22 +1,38 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('suunnitelmat/home.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        echo 'Hello World!';
     }
-    
+
     public static function filelist() {
-        View::make('filelist.html');
+        View::make('suunnitelmat/filelist.html');
     }
-    
+
     public static function file() {
-        View::make('file.html');
+        View::make('suunnitelmat/file.html');
     }
-  }
+
+    public static function upload() {
+        View::make('suunnitelmat/upload.html');
+    }
+
+    public static function editFile($id) {
+        View::make('suunnitelmat/editFile.html');
+    }
+
+    public static function editMessage($id) {
+        View::make('suunnitelmat/editMessage.html');
+    }
+
+    public static function searchPage() {
+        View::make('suunnitelmat/searchPage.html');
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,33 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/filelist', function() {
+    HelloWorldController::filelist();
+});
+
+$routes->get('/file/:id', function($id) {
+    HelloWorldController::file($id);
+});
+
+$routes->get('/upload', function() {
+    HelloWorldController::upload();
+});
+
+$routes->get('/editfile/:id', function($id) {
+    HelloWorldController::editFile($id);
+});
+
+$routes->get('/editmessage/:id', function($id) {
+    HelloWorldController::editMessage($id);
+});
+
+$routes->get('/search', function() {
+    HelloWorldController::searchPage();
+});
