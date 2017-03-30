93bb114aa0814824c817685a7ef1870f0fb0b7e8
Taneli <taneli.tiira@helsinki.fi>
2017-03-30 21:37:19 +0300

    tiedoston esittely

---
+++ app/controllers/FileController.php
@@ -0,0 +1,23 @@
+<?php
+
+class FileController extends BaseController {
+
+    public static function filelist() {
+        $files = File::all();
+        View::make('file/filelist.html', array('files' => $files));
+    }
+
+    public static function viewFile($id) {
+        $file = File::find($id);
+        View::make('file/viewFile.html', array('file' => $file));
+    }
+
+    public static function upload() {
+        View::make('file/upload.html');
+    }
+
+    public static function editFile($id) {
+        $file = File::find($id);
+        View::make('file/editFile.html', array('file' => $file));
+    }
+}
