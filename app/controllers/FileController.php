f40574c96c5eed1be6d76d9bb1a671678a9c1f96
Taneli <taneli.tiira@helsinki.fi>
2017-04-06 23:00:44 +0300

    debugging

--- app/controllers/FileController.php
+++ app/controllers/FileController.php
@@ -59,8 +59,7 @@ class FileController extends BaseController {
             $file->update();
             Redirect::to('/file/' . $file->id);
         } else {
-            print_r($validator->errors());
-            //Redirect::to('/file/' . $file->id . '/edit', array('errors' => $validator->errors()));
+            Redirect::to('/file/' . $file->id . '/edit', array('errors' => $validator->errors()));
         }
     }
     
