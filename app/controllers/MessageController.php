5e3590018080bfc137dc5916fa34603cab1b9694
Taneli <taneli.tiira@helsinki.fi>
2017-04-11 15:44:07 +0300

    fix

--- app/controllers/MessageController.php
+++ app/controllers/MessageController.php
@@ -1,29 +1,31 @@
 <?php
-
-public static function postMessage($fileId) {
-    $params = $_POST;
-    $message = new Message(array(
-        'body' => $params['body'],
-        'subject' => $params['subject']
-        'relatedFile' => File::find($fileId);
-    ));
-    $validator = $message->validator();
-    if ($validator->validate()) {
-        $message->save();
+class MessageController extends BaseController {
+    public static function postMessage($fileId) {
+        $params = $_POST;
+        $message = new Message(array(
+            'body' => $params['body'],
+            'subject' => $params['subject']
+            'relatedFile' => File::find($fileId);
+        ));
+        $validator = $message->validator();
+        if ($validator->validate()) {
+            $message->save();
+        }
     }
-}
 
-public static function editMessage($id) {
-    $params = $_POST;
-    $message = Message::find($id);
-    $message->subject = $params['subject'];
-    $message->body = $params['body'];
-    $validator = $message->validator();
-    if ($validator()->validate()) {
-        $message->update();
-        Redirect::to('/file/' . $message->relatedFile->id);
-    } else {
-        View::make('/file/' . $message->relatedFile->id . '/postmessage', array('errors' -> $validator->errors()));
+    public static function editMessage($id) {
+        $params = $_POST;
+        $message = Message::find($id);
+        $message->subject = $params['subject'];
+        $message->body = $params['body'];
+        $validator = $message->validator();
+        if ($validator()->validate()) {
+            $message->update();
+            Redirect::to('/file/' . $message->relatedFile->id);
+        } else {
+            View::make('/file/' . $message->relatedFile->id . '/postmessage', array('errors' -> $validator->errors()));
+        }
     }
+
 }
 
